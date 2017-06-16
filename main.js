f74fe7e9900eec4adb993f6573c2307066b0ee66
valmsmith39a <valmsmith39a@gmail.com>
2017-06-15 22:49:52 -0700

    build function to sort names

--- main.js
+++ main.js
@@ -1,54 +1,78 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", function() {
-  let tableRowContainer = document.getElementById("table-container");
-  let starWarsData;
+main();
 
-  function createNameHeightTable(starWarsData) {
-    const starWarsDataArr = starWarsData.results;
+function main () {
+  document.addEventListener("DOMContentLoaded", function() {
+     let tableRowContainer = document.getElementById("table-container");
 
-    for (let i = 0; i < starWarsDataArr.length; i++) {
-      let tableRow = document.createElement("TR");
-      const starWarsName = starWarsDataArr[i].name;
-      const starWarsHeight = starWarsDataArr[i].height;
+    document.getElementById("new-row-btn").addEventListener("click", function() {
+      getStarWarsData(tableRowContainer);
+    });
 
-      let tableCellName = document.createElement("TD");
-      let tableDataName = document.createTextNode(starWarsName);
-      tableCellName.appendChild(tableDataName);
-      tableRow.appendChild(tableCellName);
-
-      let tableCellHeight = document.createElement("TD");
-      let tableDataHeight = document.createTextNode(starWarsHeight);
-
-      tableCellHeight.appendChild(tableDataHeight);
-      tableRow.appendChild(tableCellHeight);
-      tableRowContainer.appendChild(tableRow);
-    }
-  }
-
-  function getStarWarsData() {
-    const starWarsDataURL = "http://swapi.co/api/people/";
-    let xmlhttp = new XMLHttpRequest();
-
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == XMLHttpRequest.DONE) {
-           if (xmlhttp.status == 200) {
-             starWarsData = JSON.parse(xmlhttp.responseText);
-             createNameHeightTable(starWarsData);
-           }
-           else if (xmlhttp.status == 400) {
-              alert('There was an error 400');
-           }
-           else {
-               alert('something else other than 200 was returned');
-           }
-        }
-    };
-    xmlhttp.open("GET", starWarsDataURL, true);
-    xmlhttp.send();
-  }
-
-  document.getElementById("new-row-btn").addEventListener("click", function() {
-    getStarWarsData();
+    document.getElementById("name-header").addEventListener("click", function() {
+      const rows = document.getElementsByClassName("row-body");
+      sortNames(rows);
+    });
   });
-});
+}
+
+const sortNames = (dataRowsNodeList) => {
+  const dataRowsArr = Array.from(dataRowsNodeList);
+  let dataRowsArrSorted = dataRowsArr.sort((a,b) => {
+    const nameA = a.firstChild.innerText.toLowerCase();
+    const nameB = b.firstChild.innerText.toLowerCase();
+    if (nameA < nameB) {
+      return -1;
+    } else if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
+  });
+  return dataRowsArrSorted;
+}
+
+const createNameHeightTable = (starWarsData, tableRowContainer) => {
+  const starWarsDataArr = starWarsData.results;
+
+  for (let i = 0; i < starWarsDataArr.length; i++) {
+    let tableRow = document.createElement("TR");
+    tableRow.setAttribute("class", "row-body");
+    const starWarsName = starWarsDataArr[i].name;
+    const starWarsHeight = starWarsDataArr[i].height;
+
+    let tableCellName = document.createElement("TD");
+    let tableDataName = document.createTextNode(starWarsName);
+    tableCellName.appendChild(tableDataName);
+    tableRow.appendChild(tableCellName);
+
+    let tableCellHeight = document.createElement("TD");
+    let tableDataHeight = document.createTextNode(starWarsHeight);
+
+    tableCellHeight.appendChild(tableDataHeight);
+    tableRow.appendChild(tableCellHeight);
+    tableRowContainer.appendChild(tableRow);
+  }
+}
+
+const getStarWarsData = (tableRowContainer) => {
+  const starWarsDataURL = "http://swapi.co/api/people/";
+  let xmlhttp = new XMLHttpRequest();
+
+  xmlhttp.onreadystatechange = function() {
+      if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+         if (xmlhttp.status == 200) {
+           const starWarsData = JSON.parse(xmlhttp.responseText);
+           createNameHeightTable(starWarsData, tableRowContainer);
+         }
+         else if (xmlhttp.status == 400) {
+            alert('There was an error 400');
+         }
+         else {
+             alert('something else other than 200 was returned');
+         }
+      }
+  };
+  xmlhttp.open("GET", starWarsDataURL, true);
+  xmlhttp.send();
+}

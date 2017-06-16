fa4fbbb0c8f13373b79c155c1918b0d986824d26
valmsmith39a <valmsmith39a@gmail.com>
2017-06-15 21:17:28 -0700

    WIP create a new row when click on button

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", function (){
-  console.log("hello world");
+document.addEventListener("DOMContentLoaded", function() {
+  let tableRowContainer = document.getElementById("table-container");
+
+  document.getElementById("new-row-btn").addEventListener("click", function() {
+    let tableRow = document.createElement("TR");
+
+    let tableCellName = document.createElement("TD");
+    let tableDataName = document.createTextNode("Luke");
+    tableCellName.appendChild(tableDataName);
+    tableRow.appendChild(tableCellName);
+
+    let tableCellHeight = document.createElement("TD");
+    let tableDataHeight = document.createTextNode("6 feet");
+
+    tableCellHeight.appendChild(tableDataHeight);
+    tableRow.appendChild(tableCellHeight);
+
+    tableRowContainer.appendChild(tableRow);
+  });
 });
